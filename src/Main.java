06c9c5d913caa74206aba18e023c1e43becff4fc
NataBarsu <khvorostukhina.n@yandex.ru>
2022-06-26 00:28:43 +0300

    Added files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_500;
+        int bonus = 20;
+        System.out.println("Начисленные мили =" + (price / bonus));
+    }
+}
+
